5deb4201eb2184758b7c6a0554c4ba10ff10389f
passerby233 <1912315910@qq.com>
2020-10-10 00:09:01 +0800

    add demos

--- demos/test_pytest_fixture_request.py
+++ demos/test_pytest_fixture_request.py
@@ -5,21 +5,95 @@
 # @FileName  : test_pytest_fixture_request.py
 # @Description  : 
 
-# 传单个参数
 import pytest
+# 案例一：传单个参数
+# 
+# 
+# @pytest.fixture()
+# def login(request):
+#     name = request.param
+#     print(f"== 账号是：{name} ==")
+#     return name
+# 
+# 
+# data = ["pyy1", "polo"]
+# ids = [f"login_test_name is:{name}" for name in data]
+# 
+# 
+# # 添加  indirect=True  参数是为了把 login 当成一个函数去执行，而不是一个参数，并且将data当做参数传入函数
+# @pytest.mark.parametrize("login", data, ids=ids, indirect=True)
+# # def test_name(login) ，这里的login是获取fixture返回的值
+# def test_name(login):
+#     print(f" 测试用例的登录账号是：{login} ")
+# 
+# 
+# # 案例二：传多个参数
+# @pytest.fixture()
+# def logins(request):
+#     param = request.param
+#     print(f"账号：{param['username']}，密码：{param['pwd']}")
+#     return param
+# 
+# 
+# data = [
+#     {"username": "name1", "pwd": "pwd1"},
+#     {"username": "name2", "pwd": "pwd2"},
+# ]
+# 
+# 
+# @pytest.mark.parametrize("logins", data, indirect=True)
+# def test_name_pwd(logins):
+#     print(f"账号是：{logins['username']}，密码是：{logins['pwd']}")
+# 
+# 
+# # 案例三：多个fixture（只加一个装饰器）
+# # 多个fixture
+# @pytest.fixture(scope="module")
+# def input_user(request):
+#     user = request.param
+#     print("登录账户：%s" % user)
+#     return user
+# 
+# 
+# @pytest.fixture(scope="module")
+# def input_psw(request):
+#     psw = request.param
+#     print("登录密码：%s" % psw)
+#     return psw
+# 
+# 
+# data = [
+#     ("name1", "pwd1"),
+#     ("name2", "pwd2")
+# ]
+# 
+# 
+# @pytest.mark.parametrize("input_user,input_psw", data, indirect=True)
+# def test_more_fixture(input_user, input_psw):
+#     print("fixture返回的内容:", input_user, input_psw)
 
 
-@pytest.fixture()
-def login(request):
-    name = request.param
-    print(f"== 账号是：{name} ==")
-    return name
+# 案例四：多个fixture（叠加装饰器）
+# 多个fixture
+@pytest.fixture(scope="function")
+def input_user(request):
+    user = request.param
+    print("登录账户：%s" % user)
+    return user
 
 
-data = ["pyy1", "polo"]
-ids = [f"login_test_name is:{name}" for name in data]
+@pytest.fixture(scope="function")
+def input_psw(request):
+    psw = request.param
+    print("登录密码：%s" % psw)
+    return psw
 
 
-@pytest.mark.parametrize("login", data, ids=ids, indirect=True)
-def test_name(login):
-    print(f" 测试用例的登录账号是：{login} ")
+name = ["name1", "name2"]
+pwd = ["pwd1", "pwd2"]
+
+
+@pytest.mark.parametrize("input_user", name, indirect=True)
+@pytest.mark.parametrize("input_psw", pwd, indirect=True)
+def test_more_fixture(input_user, input_psw):
+    print("fixture返回的内容:", input_user, input_psw)
---
+++ demos/test_pytest_html.py
@@ -0,0 +1,49 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time      : 2020/10/9 下午10:39
+# @Author    : passerby223
+# @FileName  : test_pytest_html.py
+# @Description  : 
+# 标准键盘布局
+# ┌───┐   ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┐
+# │Esc│   │ F1│ F2│ F3│ F4│ │ F5│ F6│ F7│ F8│ │ F9│F10│F11│F12│ │P/S│S L│P/B│  ┌┐    ┌┐    ┌┐
+# └───┘   └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┘  └┘    └┘    └┘
+# ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐ ┌───┬───┬───┐ ┌───┬───┬───┬───┐
+# │~ `│! 1│@ 2│# 3│$ 4│% 5│^ 6│& 7│* 8│( 9│) 0│_ -│+ =│ BacSp │ │Ins│Hom│PUp│ │N L│ / │ * │ - │
+# ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤ ├───┼───┼───┤ ├───┼───┼───┼───┤
+# │ Tab │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │{ [│} ]│ | \ │ │Del│End│PDn│ │ 7 │ 8 │ 9 │   │
+# ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤ └───┴───┴───┘ ├───┼───┼───┤ + │
+# │ Caps │ A │ S │ D │ F │ G │ H │ J │ K │ L │: ;│" '│ Enter  │               │ 4 │ 5 │ 6 │   │
+# ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────────┤     ┌───┐     ├───┼───┼───┼───┤
+# │ Shift  │ Z │ X │ C │ V │ B │ N │ M │< ,│> .│? /│  Shift   │     │ ↑ │     │ 1 │ 2 │ 3 │   │
+# ├─────┬──┴─┬─┴──┬┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤ ┌───┼───┼───┐ ├───┴───┼───┤ E││
+# │ Ctrl│    │Alt │         Space         │ Alt│    │    │Ctrl│ │ ← │ ↓ │ → │ │   0   │ . │←─┘│
+# └─────┴────┴────┴───────────────────────┴────┴────┴────┴────┘ └───┴───┴───┘ └───────┴───┴───┘
+
+import pytest
+
+
+# 案例四：多个fixture（叠加装饰器）
+# 多个fixture
+@pytest.fixture(scope="function")
+def input_user(request):
+    user = request.param
+    print("登录账户：%s" % user)
+    return user
+
+
+@pytest.fixture(scope="function")
+def input_psw(request):
+    psw = request.param
+    print("登录密码：%s" % psw)
+    return psw
+
+
+name = ["name1", "name2"]
+pwd = ["pwd1", "pwd2"]
+
+
+@pytest.mark.parametrize("input_user", name, indirect=True)
+@pytest.mark.parametrize("input_psw", pwd, indirect=True)
+def test_more_fixture(input_user, input_psw):
+    print("fixture返回的内容:", input_user, input_psw)
